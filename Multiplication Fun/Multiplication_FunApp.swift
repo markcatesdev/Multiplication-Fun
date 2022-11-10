21772648785a92c58fdb4aeaad2b3fd6b7140b2a
Mark Cates <codewithcates@gmail.com>
2022-11-09 18:22:38 -0600

    Initial Commit

---
+++ Multiplication Fun/Multiplication_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplication_FunApp.swift
+//  Multiplication Fun
+//
+//  Created by Mark Cates on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Multiplication_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
